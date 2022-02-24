cf3203d6f4bfb8e738a45d4697c693c6add2f084
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-24 16:28:35 +0200

    Fix remove function

--- src/remove.js
+++ src/remove.js
@@ -1,7 +1,7 @@
 export default function remove(ind) {
   const todoListString = localStorage.getItem('todoList');
   const todoList = JSON.parse(todoListString);
-  todoList.splice(ind - 1, 1);
+  todoList.splice(ind, 1);
   for (let i = 0; i <= todoList.length - 1; i += 1) {
     const ii = i + 1;
     todoList[i].index = ii;
